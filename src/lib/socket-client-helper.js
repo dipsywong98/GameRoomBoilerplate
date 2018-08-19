a92e2c9ef42c67dda6433fe93efb173f0c77ba04
dipsywong98 <facebookarmyoffcal@gmail.com>
2018-08-19 14:54:39 +0800

    test dynamic socket url

--- src/lib/socket-client-helper.js
+++ src/lib/socket-client-helper.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:80')
+const socket = io(window.location.href.indexOf('localhost')!==-1?'http://localhost:80':'')
 
 const subscribe = (channel, callback) => socket.on(channel,callback)
 
